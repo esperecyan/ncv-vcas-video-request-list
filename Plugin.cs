7e9541142bedbc622ad688d9c5925aa61a48005d
100の人 <100@pokemori.jp>
2022-03-30 21:15:22 +0900

    コメントからニコニコ動画、YouTubeのURLを抽出し、コメ番、ユーザーID、クリップボードへコピーボタンとともに一覧表示するように

--- Plugin.cs
+++ Plugin.cs
@@ -2,14 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
 using Plugin;
 
 namespace Esperecyan.NCVVCasVideoRequestList
 {
     public class Plugin : IPlugin
     {
+        private static readonly Regex SupportedURLPattern = new Regex(@"https?://(
+            (www\.nicovideo\.jp/watch/|nico\.ms/)[a-z]{2}[0-9]+
+            |(www\.youtube\.com/watch\?v=|youtu\.be/)[0-9A-Za-z]+
+        )", RegexOptions.IgnorePatternWhitespace);
+
         public IPluginHost Host { get; set; }
 
         public bool IsAutoRun => false;
@@ -27,6 +32,7 @@ namespace Esperecyan.NCVVCasVideoRequestList
         )).Product;
 
         private Window window;
+        private IEnumerable<UserSettingInPlugin.UserData> userDataList;
 
         public void AutoRun()
         {
@@ -45,6 +51,50 @@ namespace Esperecyan.NCVVCasVideoRequestList
                 Text = this.Name + " " + this.Version,
             };
             this.window.Show();
+            this.userDataList = this.Host.GetUserSettingInPlugin().UserDataList;
+            this.Host.ReceivedComment += this.Host_ReceivedComment;
+            this.window.FormClosing += (sender, args) => this.Host.ReceivedComment -= this.Host_ReceivedComment;
+            var dataGridView = this.window.DataGridView;
+            dataGridView.CellContentClick += (object sender, DataGridViewCellEventArgs e) =>
+            {
+                switch (dataGridView.Columns[e.ColumnIndex].Name)
+                {
+                    case "Copy":
+                        Clipboard.SetText((string)dataGridView.Rows[e.RowIndex].Cells.Cast<DataGridViewCell>()
+                            .First(cell => cell.OwningColumn.Name == "URL").Value);
+                        break;
+                }
+            };
+        }
+
+        private void Host_ReceivedComment(object sender, ReceivedCommentEventArgs e)
+        {
+
+            var rows = this.window.DataGridView.Rows;
+            foreach (var commentData in e.CommentDataList)
+            {
+                if (commentData.IsNGUser || commentData.IsNGComment
+                    || commentData.IsOfficialNGUser || commentData.IsOfficialNGWord)
+                {
+                    continue;
+                }
+
+                foreach (var match in Plugin.SupportedURLPattern.Matches(commentData.Comment).Cast<Match>())
+                {
+                    var userData = this.userDataList.FirstOrDefault(data => data.UserId == commentData.UserId);
+
+                    var index = rows.Add(new string[] {
+                        commentData.No,
+                        userData?.NickName ?? commentData.UserId,
+                        match.Value,
+                        "クリップボードへコピー",
+                    });
+                    if (userData != null)
+                    {
+                        rows[index].DefaultCellStyle.BackColor = userData.BGColor;
+                    }
+                }
+            }
         }
     }
 }
--- Window.Designer.cs
+++ Window.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Esperecyan.NCVVCasVideoRequestList
+namespace Esperecyan.NCVVCasVideoRequestList
 {
     partial class Window
     {
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.Number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UserId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.Copy = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.AllowUserToAddRows = false;
+            this.DataGridView.AllowUserToDeleteRows = false;
+            this.DataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Number,
+            this.UserId,
+            this.URL,
+            this.Copy});
+            this.DataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DataGridView.Location = new System.Drawing.Point(0, 0);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.ReadOnly = true;
+            this.DataGridView.RowTemplate.Height = 21;
+            this.DataGridView.Size = new System.Drawing.Size(800, 450);
+            this.DataGridView.TabIndex = 0;
+            // 
+            // Number
+            // 
+            this.Number.HeaderText = "コメ番";
+            this.Number.Name = "Number";
+            this.Number.ReadOnly = true;
+            this.Number.Width = 58;
+            // 
+            // UserId
+            // 
+            this.UserId.HeaderText = "ユーザーID";
+            this.UserId.Name = "UserId";
+            this.UserId.ReadOnly = true;
+            this.UserId.Width = 81;
+            // 
+            // URL
+            // 
+            this.URL.HeaderText = "URL";
+            this.URL.Name = "URL";
+            this.URL.ReadOnly = true;
+            this.URL.Width = 33;
+            // 
+            // Copy
+            // 
+            this.Copy.HeaderText = "コピー";
+            this.Copy.Name = "Copy";
+            this.Copy.ReadOnly = true;
+            this.Copy.Width = 38;
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DataGridView);
+            this.Name = "Window";
             this.Text = "Window";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        internal System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserId;
+        private System.Windows.Forms.DataGridViewLinkColumn URL;
+        private System.Windows.Forms.DataGridViewButtonColumn Copy;
     }
 }
