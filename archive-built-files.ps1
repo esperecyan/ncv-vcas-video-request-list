5a9389896c7061f992e6f284c88b4dcceef5816b
100の人 <100@pokemori.jp>
2023-01-14 14:50:02 +0900

    Releaseビルド時、ビルド後のファイルのZIPアーカイブをデスクトップへ作成するように

---
+++ archive-built-files.ps1
@@ -0,0 +1,25 @@
+<#
+.SYNOPSIS
+	GitHub Releaseページへのアップロード用に、ビルド後のファイルを、ZIPアーカイブへまとめます。
+.DESCRIPTION
+	ZIPファイルの出力先は、デスクトップです。
+	ZIPファイル名は、ビルドしたファイルの出力先フォルダ名です。
+	同一パスのファイルがあった場合は上書きします。
+.PARAMETER TargetDir
+	ビルドしたファイルの出力先フォルダのパス。
+#>
+param([string]$TargetDir)
+
+$OUTPUT_FILE_NAMES = @(
+	'HtmlAgilityPack - LICENSE',
+	'HtmlAgilityPack.dll',
+	'NCVVCasVideoRequestList.dll',
+	'NCVVCasVideoRequestList.URL'
+)
+
+$DESTINATION_FOLDER_PATH = [Environment]::GetFolderPath('Desktop')
+
+Compress-Archive `
+	-LiteralPath ($OUTPUT_FILE_NAMES | ForEach-Object { Join-Path $TargetDir $_ } ) `
+	-DestinationPath (Join-Path $DESTINATION_FOLDER_PATH ((Split-Path $TargetDir -Leaf) + '.zip')) `
+	-Force
